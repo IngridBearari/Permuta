ff89869f369a8aeff8815f17445ca1a7a5f10790
IngridBearari <bearariingrid@gmail.com>
2025-05-02 14:39:37 -0300

    Update MensagemDespedida.java

--- src/factory/MensagemDespedida.java
+++ src/factory/MensagemDespedida.java
@@ -2,6 +2,6 @@ package src.factory;
 
 public class MensagemDespedida implements Mensagem {
     public String formatar(String nome) {
-        return "Até logo, " + nome + "!";
+        return "Ate logo, " + nome + "!";
     }
 }
